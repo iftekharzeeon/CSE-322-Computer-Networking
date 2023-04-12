da87620f226a6d1dde69b48649460a3a1cc1d2e9
Iftekhar E Mahbub Zeeon <1805038@ugrad.cse.buet.ac.bd>
2023-04-13 03:45:43 +0600

    Folder restructured

--- NS2 Project/wired.tcl
+++ NS2 Project/wired.tcl
@@ -1,5 +1,22 @@
 #Create a simulator object
 
+set ns [new Simulator]
+
+# Number of nodes
+set val(nn) [lindex $argv 0]
+
+# Number of flows
+set val(nf) [lindex $argv 1] 
+
+# Packet Rate
+set val(pr) [lindex $argv 2] 
+
+set val(qlimit) 100
+set val(start_time) 1.0
+set val(end_time) 150
+
+# set val(seed) 23
+
 # =======================================================================
 Queue/RED set thresh_queue_ 10
 Queue/RED set maxthresh_queue_ 40
@@ -8,12 +25,10 @@ Queue/RED set bytes_ false
 Queue/RED set queue_in_bytes_ false
 Queue/RED set gentle_ false
 Queue/RED set mean_pktsize_ 100
-Queue/RED set modified_red_ 1
+Queue/RED set modified_red_ [lindex $argv 3] 
 Queue/RED set buffer_size 80
 Queue/RED set cur_max_p_ 0.1
 # =======================================================================
-
-set ns [new Simulator]
 
 #
 # Create a simple six node topology:
@@ -28,102 +43,76 @@ set ns [new Simulator]
 #
 
 
-#Define different colors for data flows (for NAM)
-$ns color 1 Blue
-$ns color 2 Red
-$ns color 3 Yellow
-
 #Open the NAM file and trace file
 set nam_file [open animation_wired.nam w]
 set trace_file [open trace_wired.tr w]
 $ns namtrace-all $nam_file
 $ns trace-all $trace_file
 
-#Create six nodes
-set node_s1 [$ns node]
-set node_s2 [$ns node]
-set node_s3 [$ns node]
-set node_r1 [$ns node]
-set node_r2 [$ns node]
-set node_d1 [$ns node]
-set node_d2 [$ns node]
-set node_d3 [$ns node]
+# Create 2 routers
+set node_(r1) [$ns node]
+set node_(r2) [$ns node]
 
-#Create links between the nodes
-# ns <link-type> <node1> <node2> <bandwidht> <delay> <queue-type-of-node2>
-$ns duplex-link $node_s1 $node_r1 15Mb 10ms RED 
-$ns duplex-link $node_s2 $node_r1 15Mb 10ms RED 
-$ns duplex-link $node_s3 $node_r1 15Mb 10ms RED 
-$ns duplex-link $node_r1 $node_r2 1Mb 30ms RED 
-$ns queue-limit $node_r1 $node_r2 100
-$ns queue-limit $node_r2 $node_r1 100
-$ns duplex-link $node_d1 $node_r2 15Mb 30ms RED 
-$ns duplex-link $node_d2 $node_r2 15Mb 30ms RED 
-$ns duplex-link $node_d3 $node_r2 15Mb 30ms RED 
+# Update node numbers
+set val(nn) [expr {$val(nn) - 2}]
 
-#Give node position (for NAM)
-$ns duplex-link-op $node_s1 $node_r1 orient right-down
-$ns duplex-link-op $node_s2 $node_r1 orient right-down
-$ns duplex-link-op $node_s3 $node_r1 orient right-down
-$ns duplex-link-op $node_r1 $node_r2 orient right
-$ns duplex-link-op $node_r1 $node_r2 queuePos 0
-$ns duplex-link-op $node_r2 $node_r1 queuePos 0
-$ns duplex-link-op $node_d1 $node_r2 orient left-up
-$ns duplex-link-op $node_d2 $node_r2 orient left-up
-$ns duplex-link-op $node_d3 $node_r2 orient left-up
+puts "Number of nodes without routers $val(nn)"
 
+# Create links between the routers
+$ns duplex-link $node_(r1) $node_(r2) 1Mb 30ms RED 
 
-#Setup a TCP connection 1
-#Setup a flow
-set tcp1 [new Agent/TCP]
-$ns attach-agent $node_s1 $tcp1
-set sink1 [new Agent/TCPSink]
-$ns attach-agent $node_d1 $sink1
-$ns connect $tcp1 $sink1
-$tcp1 set fid_ 1
+# Set queue limit for the routers
+$ns queue-limit $node_(r1) $node_(r2) $val(qlimit)
+$ns queue-limit $node_(r2) $node_(r1) $val(qlimit)
+$ns duplex-link-op $node_(r1) $node_(r2) orient right
+$ns duplex-link-op $node_(r1) $node_(r2) queuePos 0
+$ns duplex-link-op $node_(r2) $node_(r1) queuePos 0
 
-#Setup a FTP Application over TCP connection 1
-set ftp1 [new Application/FTP]
-$ftp1 attach-agent $tcp1
-$ftp1 set type_ FTP
+# create nodes
+for {set i 0} {$i < [expr {$val(nn) / 2}]} {incr i} {
 
+    # Source Nodes
+    set node_(s$i) [$ns node]
+    # Create link between the source nodes & r1
+    $ns duplex-link $node_(s$i) $node_(r1) 15Mb 10ms RED 
 
-#Setup a TCP connection 2
-#Setup a flow
-set tcp2 [new Agent/TCP]
-$ns attach-agent $node_s2 $tcp2
-set sink2 [new Agent/TCPSink]
-$ns attach-agent $node_d2 $sink2
-$ns connect $tcp2 $sink2
-$tcp2 set fid_ 2
+    # Destination Nodes
+    set node_(d$i) [$ns node] 
+    # Create link between the dest nodes & r2
+    $ns duplex-link $node_(d$i) $node_(r2) 15Mb 10ms RED
+}
 
-#Setup a FTP Application over TCP connection 2
-set ftp2 [new Application/FTP]
-$ftp2 attach-agent $tcp2
-$ftp2 set type_ FTP
+set val(max) [expr {$val(nn) / 2}]
+set val(min) 0
 
-#Setup a TCP connection 3
-#Setup a flow
-set tcp3 [new Agent/TCP]
-$ns attach-agent $node_s3 $tcp3
-set sink3 [new Agent/TCPSink]
-$ns attach-agent $node_d3 $sink3
-$ns connect $tcp3 $sink3
-$tcp3 set fid_ 3
+# expr srand($val(seed))
 
-#Setup a FTP Application over TCP connection 3
-set ftp3 [new Application/FTP]
-$ftp3 attach-agent $tcp3
-$ftp3 set type_ FTP
+#Setup flows
 
-#Schedule events for the CBR and FTP agents
-$ns at 0.1 "$ftp1 start"
-$ns at 0.5 "$ftp2 start"
-$ns at 0.9 "$ftp3 start"
-$ns at 4.0 "$ftp1 stop"
-$ns at 4.5 "$ftp2 stop"
-$ns at 4.9 "$ftp3 stop"
+for {set i 0} {$i < $val(nf)} {incr i} {
 
+    set src [expr int(rand() * ($val(max)-$val(min)))]
+    set dest [expr int(rand() * ($val(max)-$val(min)))]
+    
+
+    set tcp [new Agent/TCP]
+    $ns attach-agent $node_(s$src) $tcp
+    set sink [new Agent/TCPSink]
+    $ns attach-agent $node_(d$dest) $sink
+    $ns connect $tcp $sink
+    $tcp set fid_ $i
+
+    $tcp set packetSize_ 1000
+    $tcp set window_ [expr 10 *($val(pr) / 100)]
+
+    set ftp [new Application/FTP]
+    $ftp attach-agent $tcp
+    $ftp set type_ FTP
+
+    $ns at $val(start_time) "$ftp start"
+    $ns at $val(end_time) "$ftp stop"
+    
+}
 
 #Define a 'finish' procedure
 proc finish {} {
@@ -142,7 +131,7 @@ proc finish {} {
 # $ns at 4.9 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
 
 #Call the finish procedure after 5 seconds of simulation time
-$ns at 5.0 "finish"
+$ns at $val(end_time) "finish"
 
 #Run the simulation
 $ns run
--- NS2 Project/wireless.tcl
+++ NS2 Project/wireless.tcl
@@ -1,7 +1,6 @@
 # simulator
 set ns [new Simulator]
 
-
 # ======================================================================
 # Define options
 
@@ -10,32 +9,52 @@ set val(prop)         Propagation/TwoRayGround ;# radio-propagation model
 set val(ant)          Antenna/OmniAntenna      ;# Antenna type
 set val(ll)           LL                       ;# Link layer type
 set val(ifq)          Queue/RED  ;# Interface queue type
-set val(ifqlen)       50                       ;# max packet in ifq
-set val(netif)        Phy/WirelessPhy          ;# network interface type
-set val(mac)          Mac/802_11               ;# MAC type
+set val(ifqlen)       100                       ;# max packet in ifq
+set val(netif)        Phy/WirelessPhy        ;# network interface type
+set val(mac)          Mac/802_11            ;# MAC type
 set val(rp)           DSDV                     ;# ad-hoc routing protocol 
-set val(nn)           20                       ;# number of mobilenodes
 # =======================================================================
 
-Queue/RED set thresh_ 10
-Queue/RED set maxthresh_ 30
-Queue/RED set q_weight_ 0.002
+# Number of nodes
+set val(nn) [lindex $argv 0]
+
+# Number of flows
+set val(nf) [lindex $argv 1]
+
+# Area size
+set val(as) [lindex $argv 2]
+
+# Packet Rate
+set val(pr) [lindex $argv 3]
+
+set ftp_interval [expr 1.0/$val(pr)]
+
+# set val(seed) 23
+
+# =======================================================================
+Queue/RED set thresh_queue_ 10
+Queue/RED set maxthresh_queue_ 40
+Queue/RED set q_weight_ 0.003
 Queue/RED set bytes_ false
 Queue/RED set queue_in_bytes_ false
 Queue/RED set gentle_ false
 Queue/RED set mean_pktsize_ 1000
+Queue/RED set modified_red_ [lindex $argv 4]
+Queue/RED set buffer_size 80
+Queue/RED set cur_max_p_ 0.1
+# =======================================================================
 
 # trace file
-set trace_file [open trace.tr w]
+set trace_file [open trace_wireless.tr w]
 $ns trace-all $trace_file
 
 # nam file
-set nam_file [open animation.nam w]
-$ns namtrace-all-wireless $nam_file 500 500
+set nam_file [open animation_wireless.nam w]
+$ns namtrace-all-wireless $nam_file $val(as) $val(as)
 
 # topology: to keep track of node movements
 set topo [new Topography]
-$topo load_flatgrid 500 500 ;# 500m x 500m area
+$topo load_flatgrid $val(as) $val(as)
 
 
 # general operation director for mobilenodes
@@ -64,6 +83,7 @@ create-god $val(nn)
 #                  -routerTrace    ON or OFF
 #                  -macTrace       ON or OFF
 #                  -movementTrace  ON or OFF
+#                 -energyModel    "EnergyModel" \
 
 # ======================================================================
 
@@ -76,66 +96,101 @@ $ns node-config -adhocRouting $val(rp) \
                 -propType $val(prop) \
                 -phyType $val(netif) \
                 -topoInstance $topo \
-                -channelType $val(chan) \
+                -channel [new $val(chan)] \
                 -agentTrace ON \
                 -routerTrace ON \
                 -macTrace OFF \
-                -movementTrace OFF
+                -movementTrace OFF \
+                -energyModel "EnergyModel" \
+                -initialEnergy  1000.0 \
+                -rxPower        1.0 \
+                -txPower        1.0 \
+                -idlePower 1.0 \
+                -sleepPower 0.001 \
+
+set val(max) $val(as)
+set val(min) 1
+
+# expr srand($val(seed))
 
 # create nodes
-for {set i 0} {$i < $val(nn) } {incr i} {
+for {set i 0} {$i < $val(nn)} {incr i} {
     set node($i) [$ns node]
     $node($i) random-motion 0       ;# disable random motion
 
-    $node($i) set X_ [expr (500 * $i) / $val(nn)]
-    $node($i) set Y_ [expr (500 * $i) / $val(nn)]
+    # Randomly put x axes and y axes
+    $node($i) set X_ [expr int(rand() * ($val(max)-$val(min))) + $val(min)]
+    $node($i) set Y_ [expr int(rand() * ($val(max)-$val(min))) + $val(min)]
     $node($i) set Z_ 0
 
     $ns initial_node_pos $node($i) 20
+    
 } 
 
-
-
-
+set val(max) $val(nn)
+set val(min) 0
 
 # Traffic
-set val(nf)         10                ;# number of flows
+# Initialize one random source
+set src [expr int(rand() * ($val(max)-$val(min))) + $val(min)]
+
+puts $src
 
 for {set i 0} {$i < $val(nf)} {incr i} {
-    set src $i
-    set dest [expr $i + 10]
+
+    # picking random destination/sink node
+    set dest [expr int(rand() * ($val(max)-$val(min))) + $val(min)]
+    # puts $dest
+    while {$src == $dest} {
+        set dest [expr int(rand() * ($val(max)-$val(min))) + $val(min)]
+    }
+
+    # puts "$src >> $dest" 
+    # puts "-----------------------"
 
     # Traffic config
     # create agent
     set tcp [new Agent/TCP]
     set tcp_sink [new Agent/TCPSink]
+
+    # /home/zeeon/ns-allinone-2.35/nam-1.15/nam animation_wired.nam
+
     # attach to nodes
     $ns attach-agent $node($src) $tcp
     $ns attach-agent $node($dest) $tcp_sink
+
+    $tcp set packetSize_ 1000
+    $tcp set window_ [expr 10 *($val(pr) / 100)]
+    
     # connect agents
     $ns connect $tcp $tcp_sink
     $tcp set fid_ $i
 
+    # $tcp set window_ 15
+    # $tcp set packetRate_ $val(pr)
+
     # Traffic generator
     set ftp [new Application/FTP]
     # attach to agent
+
+    # puts "Interval $ftp_interval"
+
     $ftp attach-agent $tcp
-    
-    # start traffic generation
+
+    # starting traffic/flow generation
+    # $ns at [expr int(5 * rand()) + 1] "$ftp start"
     $ns at 1.0 "$ftp start"
 }
 
 
-
 # End Simulation
-
-# Stop nodes
+# stopping nodes
 for {set i 0} {$i < $val(nn)} {incr i} {
-    $ns at 50.0 "$node($i) reset"
+    $ns at 150.0 "$node($i) reset"
 }
 
-# call final function
-proc finish {} {
+# defining terminating procedures
+proc finish_simulation {} {
     global ns trace_file nam_file
     $ns flush-trace
     close $trace_file
@@ -144,16 +199,15 @@ proc finish {} {
 
 proc halt_simulation {} {
     global ns
-    puts "Simulation ending"
+    puts "Simulation Ending"
+    puts "____________________________"
     $ns halt
 }
 
-$ns at 50.0001 "finish"
-$ns at 50.0002 "halt_simulation"
-
-
+$ns at 150.0001 "finish_simulation"
+$ns at 150.0002 "halt_simulation"
 
 # Run simulation
 puts "Simulation starting"
+puts "^^^^^^^^^^^^^^^^^^^^^^^^^"
 $ns run
-
